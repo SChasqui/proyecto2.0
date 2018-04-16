21b2eaa92bf51b96e21952e80cd1a8df826de990
santi <santi@172.30.100.229>
2018-04-16 15:10:03 -0500

    Buenas tardes, si ven una clase con mi nombre es porque subió ;v

---
+++ Proyecto-Final/src/modelo/SiVenEstoChasquiEspro.java
@@ -0,0 +1,6 @@
+package modelo;
+
+public class SiVenEstoChasquiEspro {
+	//Chasqui ha estado jugando un poco con eclipse para el bien del equipo
+	//Así que si ven esto, Chasqui efectivamente es muy pro
+}
