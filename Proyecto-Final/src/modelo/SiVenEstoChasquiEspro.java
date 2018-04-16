b67f87bd374f3e6166c59fe95e6336302e606fd0
ZeroN <ZeroN@172.30.174.105>
2018-04-16 17:23:45 -0500

    chasqui lo lograste ?

--- Proyecto-Final/src/modelo/SiVenEstoChasquiEspro.java
+++ Proyecto-Final/src/modelo/SiVenEstoChasquiEspro.java
@@ -1,6 +1,5 @@
 package modelo;
 
 public class SiVenEstoChasquiEspro {
-	//Chasqui ha estado jugando un poco con eclipse para el bien del equipo
-	//Así que si ven esto, Chasqui efectivamente es muy pro
+	// Chasqui es anti pro
 }
