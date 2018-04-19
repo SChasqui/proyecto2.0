93668742e574cb22c797c3540afa96c9671bc33d
ZeroN <ZeroN@LAPTOP-RFT9DKIC>
2018-04-19 12:04:08 -0500

    Multi tecleo avtivado :v

--- Proyecto-Final/src/interfaz/PanelPruebas.java
+++ Proyecto-Final/src/interfaz/PanelPruebas.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
@@ -20,6 +23,9 @@ public class PanelPruebas extends JDialog implements KeyListener{
 	public final static int	FLECHA_DERECHA = 39;
 	public final static int	FLECHA_ABAJO = 38;
 	public final static int FLECHA_IZQUIERDA = 37;
+	
+	
+	private final Set<Integer> pressed = new HashSet<Integer>();
 	
 	//--------------------------------------
 	// Relaciones
@@ -54,32 +60,64 @@ public class PanelPruebas extends JDialog implements KeyListener{
 	}
 	
 	
+//	@Override
+//	public void keyTyped(KeyEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void keyPressed(KeyEvent e) {
+//		 System.out.println("Codigo de tecla : " + e.getKeyCode());
+//		 if(e.getKeyCode() == FLECHA_IZQUIERDA) {
+//			 ventana.darJuego().darJugador1().darPersonaje().moverX(-4);
+//		 }else if(e.getKeyCode() == FLECHA_ABAJO) {
+//			 ventana.darJuego().darJugador1().darPersonaje().moverY(-4);
+//		 }else if(e.getKeyCode() == FLECHA_DERECHA) {
+//			 ventana.darJuego().darJugador1().darPersonaje().moverX(4);
+//		 }else if(e.getKeyCode() == FLECHA_ARRIBA) {
+//			 ventana.darJuego().darJugador1().darPersonaje().moverY(4);
+//		 }
+//		 
+//		 repaint();
+//	}
+	
 	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    public synchronized void keyPressed(KeyEvent e) {
+        pressed.add(e.getKeyCode());
+        System.out.println((int)e.getKeyChar()+" ");
+        if (pressed.size() > 0) {
+          for (int c : pressed){
+        	  
+        	  if(c == FLECHA_IZQUIERDA) {
+     			 ventana.darJuego().darJugador1().darPersonaje().moverX(-4);
+     		 }else if(c == FLECHA_ABAJO) {
+     			 ventana.darJuego().darJugador1().darPersonaje().moverY(-4);
+     		 }else if(c == FLECHA_DERECHA) {
+     			 ventana.darJuego().darJugador1().darPersonaje().moverX(4);
+     		 }else if(c == FLECHA_ARRIBA) {
+     			 ventana.darJuego().darJugador1().darPersonaje().moverY(4);
+     		 }
+     		 
+     		 repaint();
+        	  
+        	  
+          }
+        }
+    }
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		 System.out.println("Codigo de tecla : " + e.getKeyCode());
-		 if(e.getKeyCode() == FLECHA_IZQUIERDA) {
-			 ventana.darJuego().darJugador1().darPersonaje().moverX(-4);
-		 }else if(e.getKeyCode() == FLECHA_ABAJO) {
-			 ventana.darJuego().darJugador1().darPersonaje().moverY(-4);
-		 }else if(e.getKeyCode() == FLECHA_DERECHA) {
-			 ventana.darJuego().darJugador1().darPersonaje().moverX(4);
-		 }else if(e.getKeyCode() == FLECHA_ARRIBA) {
-			 ventana.darJuego().darJugador1().darPersonaje().moverY(4);
-		 }
-		 
-		 repaint();
-	}
+    @Override
+    public synchronized void keyReleased(KeyEvent e) {
+        pressed.remove(e.getKeyCode());
+    }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void keyTyped(KeyEvent e) {/* Not used */ }
+
+//	@Override
+//	public void keyReleased(KeyEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 	
 }
