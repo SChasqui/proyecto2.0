ea35bfb05030189915eff81e99c558d53d6b5b95
ZeroN <ZeroN@172.30.161.156>
2018-05-24 11:33:51 -0500

    No se que probar, todos los metodos son demaciado visuales

--- Proyecto-Final/pruebas/modeloTest/PersonajeTest.java
+++ Proyecto-Final/pruebas/modeloTest/PersonajeTest.java
@@ -5,10 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import modelo.Personaje;
+
 class PersonajeTest {
+	
+	/*
+	 * Informacion
+	 * [0] ----> Bardock --> $800P
+	 * [1] ----> Beerus --> $1000P
+	 * [2] ----> Broly --> $1000P
+	 * [3] ----> Frieza --> $900P
+	 * [4] ----> GohanSSJ_Kid --> $1000P
+	 * [5] ----> Goku --> $800P
+	 * [6] ----> 18 --> $900P
+	 * [7] ----> Goku_Red --> $1000P
+	 * [8] ----> Kid_Buu --> $1000P
+	 * [9] ----> Vegeta --> $900P 
+	 */
+	
+	private Personaje aProbar;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		aProbar = new Personaje("Goku_Red", 1000, 7);
 	}
 
 	@Test
