58de189b241fd170c86fe092c1794c3325b087c1
ZeroN <ZeroN@172.30.161.156>
2018-05-24 11:26:14 -0500

    xD

--- Proyecto-Final/pruebas/modeloTest/PersonajeTest.java
+++ Proyecto-Final/pruebas/modeloTest/PersonajeTest.java
@@ -1,9 +1,11 @@
+package modeloTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class modeloTest {
+class PersonajeTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
